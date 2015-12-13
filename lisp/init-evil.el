5ebee37b122d7703e2e3533eaa9fdc6c8a03d9dd
xueeinstein <xueeinstein@gmail.com>
2015-12-13 11:17:23 +0800

    setup for evil-nerd-commenter

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -6,13 +6,27 @@
 (global-evil-leader-mode)
 (evil-mode 1)
 
+;; setup for evil-nerd-commenter
+;; Emacs key bindings
+(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+(global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+(global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+(global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+
 (evil-leader/set-leader ",")
 (evil-leader/set-key
   "e" 'find-file
   "b" 'switch-to-buffer
   "k" 'kill-buffer
   "s" 'split-window-vertically
-  "v" 'split-window-horizontally)
+  "v" 'split-window-horizontally
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line)
 
 (eval-after-load 'hideshow
   '(progn

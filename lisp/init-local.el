36aee7ea985b5478509659285eb116d8928cfa23
xueeinstein <xueeinstein@gmail.com>
2016-05-09 23:26:02 +0800

    add function to config elpy using python2 or python3

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,4 +2,14 @@
 (elpy-enable)
 (elpy-use-ipython)
 
+(defun elpy-python2 ()
+  (interactive)
+  (setq elpy-rpc-python-command "python2")
+  (setq python-shell-interpreter "ipython2"))
+
+(defun elpy-python3 ()
+  (interactive)
+  (setq elpy-rpc-python-command "python3")
+  (setq python-shell-interpreter "ipython3"))
+
 (provide 'init-local)

1d4361ead76b3d3a4c8006cae5f8cfa3326c2e94
xueeinstein <xueeinstein@gmail.com>
2016-09-04 17:48:41 +0800

    add matlab-mode as a third part lib

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
 
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,1 +1,26 @@
+;; ================================
+;; matlab mode
+;; ================================
+(autoload 'matlab-mode "matlab" "Enter Matlab mode" t)
+(setq auto-mode-alist (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
+(setq matlab-shell-command "/home/billxue/bin/matlab-cli")
+(autoload 'matlab-shell "matlab" "Interactive Matlab mode" t)
+
+(defun my-matlab-mode-hook ()
+  (setq matlab-function-indent t)              ; if you want function bodies indented
+  (setq fill-column 76)                        ; where auto-fill should wrap
+  (font-lock-mode 1)
+  (turn-on-auto-fill)
+  (matlab-mode-hilit)
+  )
+(setq matlab-mode-hook 'my-matlab-mode-hook)
+
+(defun my-matlab-shell-mode-hook ()
+  (setq matlab-function-indent t)              ; if you want function bodies indented
+  (setq fill-column 76)                        ; where auto-fill should wrap
+  (font-lock-mode 1)
+  )
+(setq matlab-shell-mode-hook 'my-matlab-shell-mode-hook)
+
+
 (provide 'init-local)

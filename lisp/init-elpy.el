4b408b5af475e063752f8adf4acd08028d34980b
xueeinstein <xueeinstein@gmail.com>
2017-02-02 23:19:07 +0800

    [fix] IPython 5 not compatible with emacs

--- lisp/init-elpy.el
+++ lisp/init-elpy.el
@@ -9,12 +9,14 @@
 (defun elpy-python2 ()
   (interactive)
   (setq elpy-rpc-python-command "python2")
-  (setq python-shell-interpreter "ipython2"))
+  (setq python-shell-interpreter "ipython2")
+  (setq python-shell-interpreter-args "--simple-prompt -i"))
 
 (defun elpy-python3 ()
   (interactive)
   (setq elpy-rpc-python-command "python3")
-  (setq python-shell-interpreter "ipython3"))
+  (setq python-shell-interpreter "ipython3")
+  (setq python-shell-interpreter-args "--simple-prompt -i"))
 
 ;; let windmove.el bind to these key
 (define-key elpy-mode-map (kbd "<C-right>") nil)

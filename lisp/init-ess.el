71dfb7dedbefc88d30f8ce4c2712ce1a48c9c1d4
xueeinstein <xueeinstein@gmail.com>
2017-02-05 11:43:34 +0800

    show line number in ess

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -3,4 +3,8 @@
 ;; So auto-complete works with TAB
 (define-key ess-mode-map "\t" nil)
 (define-key ess-mode-map "\C-c\t" 'ess-indent-or-complete)
+
+;; line number
+(add-hook 'ess-mode-hook 'linum-mode)
+
 (provide 'init-ess)

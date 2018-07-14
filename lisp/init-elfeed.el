317e09fd7ec230a5559ba01c812e6dd96b169352
Bill Xue <xueeinstein@gmail.com>
2018-07-14 09:11:05 +0800

    add customized function to copy elfeed title-link

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -11,4 +11,22 @@
 (elfeed-org)
 (setq rmh-elfeed-org-files (list "~/org/elfeed.org"))
 
+;; copy elfeed link as org link
+(after-load 'elfeed
+  (defun elfeed-link-title (entry)
+    "Copy the entry title and URL as org link to the clipboard."
+    (interactive)
+    (let* ((link (elfeed-entry-link entry))
+           (title (elfeed-entry-title entry))
+           (titlelink (org-make-link-string link title)))
+      (when titlelink
+        (kill-new titlelink)
+        (x-set-selection 'PRIMARY titlelink)
+        (message "Yanked: %s" titlelink))))
+  (defun elfeed-show-link-title ()
+    "Copy the current entry title and URL as org link to the clipboard."
+    (interactive)
+    (elfeed-link-title elfeed-show-entry))
+  (define-key elfeed-show-mode-map (kbd "C-x y") 'elfeed-show-link-title))
+
 (provide 'init-elfeed)

9abe6e1137f88c1fdcdae98ae37f1023cb2460b6
Bill Xue <xueeinstein@gmail.com>
2019-03-07 15:52:20 +0800

    update default *Ilist* window size

--- lisp/init-imenu.el
+++ lisp/init-imenu.el
@@ -1,7 +1,8 @@
 (require 'imenu-list)
 
 (after-load 'imenu-list
-  (global-set-key (kbd "M-'") 'imenu-list-smart-toggle))
+  (global-set-key (kbd "M-'") 'imenu-list-smart-toggle)
+  (setf imenu-list-size 0.18))
 
 (after-load 'helm
   (global-set-key (kbd "C-'") 'helm-imenu))

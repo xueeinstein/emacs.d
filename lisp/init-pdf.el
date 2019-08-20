04541ce1da0145c537888e8417a3b4a7379b5e37
Bill Xue <xueeinstein@gmail.com>
2019-08-20 12:43:29 +0800

    enable vim like control

--- lisp/init-pdf.el
+++ lisp/init-pdf.el
@@ -1,4 +1,9 @@
 (require 'pdf-tools)
+(require 'pdf-view)
 (pdf-tools-install)
+
+(setq pdf-view-continuous t)
+(define-key pdf-view-mode-map (kbd "j") 'pdf-view-next-line-or-next-page)
+(define-key pdf-view-mode-map (kbd "k") 'pdf-view-previous-line-or-previous-page)
 
 (provide 'init-pdf)

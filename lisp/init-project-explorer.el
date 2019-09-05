a6ab231f1377253608ad4f306351f8e39f206ff8
Bill Xue <xueeinstein@gmail.com>
2019-09-05 13:56:56 +0800

    repace neotree with project-explorer

---
+++ lisp/init-project-explorer.el
@@ -0,0 +1,6 @@
+(require 'project-explorer)
+(global-set-key (kbd "<f8>") 'project-explorer-open)
+(global-set-key (kbd "s-<f8>") 'project-explorer-helm)
+(setq pe/width 30)
+
+(provide 'init-project-explorer)
